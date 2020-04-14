a4a284766f85c777daafbc4fda20084aa68e58b1
Michael Khalil <mikegeorge2012@gmail.com>
2020-04-14 06:23:28 +0200

    Major edits, and adding the synchronizing phase before computing D

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def emg_decompostion(original_signal, moving_avg):
     signal_after_rectify[np.where(signal_after_rectify < 0)] = signal[np.where(signal < 0)] * -1
 
     # Set threshold
-    threshold = 3 * np.std(signal[0:280])
+    threshold = 3 * np.std(signal[0:100])
 
     # Moving Average
     signalTemp = np.zeros(signal.shape)
@@ -26,7 +26,8 @@ def emg_decompostion(original_signal, moving_avg):
     muTemplates = []
     muapTimestamps = []
     muapClasses = []
-                  
+    
+    detection_signal = np.zeros(signal.shape)
     for i in range(0,signal.shape[0]):
         # Skip detection checking when still in the range of the previous MUAP (wait till signal is less than threshold)
         if doSkip:
@@ -35,11 +36,15 @@ def emg_decompostion(original_signal, moving_avg):
             else:
                 doSkip = False
                 
-        tmp = signal_after_average[i:i+moving_avg] 
+        tmp = np.copy(signal_after_average[i:i+moving_avg])
         isMUAP = all(tmp > threshold)
         if(isMUAP):
             muapTimestamps.append(i)
-            tmp = signal[i:i+moving_avg] 
+            detection_signal[i] = signal[i]
+            
+            peak_index = np.where(signal[i-moving_avg+1:i+2] == np.max(signal[i-moving_avg+1:i+2]))[0][0]
+            peak_shift = peak_index - int(moving_avg / 2)
+            tmp = np.copy(signal[i-moving_avg+peak_shift+1:i+peak_shift+1])
 
             if (len(muTemplates) == 0):
                 muTemplates.append(tmp)
@@ -59,64 +64,58 @@ def emg_decompostion(original_signal, moving_avg):
                 if(not(classified)):  #create a new template in muTemplates
                     muapClasses.append(len(muTemplates))
                     muTemplates.append(tmp)
-                    
             
-            i += moving_avg
             if signal_after_average[i] > threshold:
                 doSkip = True
         
-    muapPeaks = []
-    for i in range(len(muapTimestamps)):
-        max = signal[muapTimestamps[i]]
-        maxIndex = muapTimestamps[i]
-        for j in range(muapTimestamps[i], muapTimestamps[i]+moving_avg):
-            if(signal[j] > max):
-                max = signal[j]
-                maxIndex = j
-        muapPeaks.append(maxIndex)
+    # muapPeaks = []
+    # for i in range(len(muapTimestamps)):
+    #     max = signal[muapTimestamps[i]]
+    #     maxIndex = muapTimestamps[i]
+    #     for j in range(muapTimestamps[i], muapTimestamps[i]+moving_avg):
+    #         if(signal[j] > max):
+    #             max = signal[j]
+    #             maxIndex = j
+    #     muapPeaks.append(maxIndex)
 
-    return signal, signal_after_average, signal_after_rectify, muapPeaks, muTemplates
+    return detection_signal, signal_after_average, signal_after_rectify, np.array(muapTimestamps), muTemplates, np.array(muapClasses)
 
 
+start = 30000
+end = 35000
 original_signal = read_file('Data.txt')
-emg, signal_avg, signal_rectify, timestamps, templates = emg_decompostion(original_signal,20)
-# plt.plot(original_signal)
+detection_signal, signal_avg, signal_rectify, timestamps, templates, classes = emg_decompostion(original_signal[start:end],20)
+
+
+# # Draw Templates
+# fig, x = plt.subplots(len(templates),1)
+# for i in range(len(templates)):
+#     x[i].plot(templates[i])
 # plt.show()
 
-fig, x = plt.subplots(len(templates),1)
+# Draw detections
+plt.plot(original_signal[start:end])
+# timestamps = timestamps[np.where(timestamps >= start)]
+# timestamps = timestamps[np.where(timestamps <= end)]
+# classes = classes[np.where(timestamps >= start)]
+# classes = classes[np.where(timestamps <= end)]
 for i in range(len(templates)):
-    x[i].plot(templates[i])
+    plt.plot(timestamps[np.where(classes == i)[0]], np.ones(timestamps[np.where(classes == i)[0]].shape) * 900, "*")
+    # plt.plot(timestamps[np.where(classes == i)[0]] - start, np.ones(timestamps[np.where(classes == i)[0]].shape) * 900, "*")
 plt.show()
 
-#fig, x = plt.subplots(4,1)
-#x[0].plot(original_signal[30000:35000])
-#x[0].title.set_text("Original Signal")
-#x[1].plot(signal_rectify[30000:35000])
-#x[1].title.set_text("Rectify")
-#x[2].plot(signal_avg[30000:35000])
-#x[2].title.set_text("Moving Average")
-#x[3].plot(detected_signal[30000:35000])
-#x[3].title.set_text("Det")
-#plt.show()
 
+# # Extra
 # fig, x = plt.subplots(4,1)
-# x[0].plot(original_signal[0:2000])
-# x[0].title.set_text("Original Signal")
-# x[1].plot(signal_rectify[0:2000])
-# x[1].title.set_text("Rectify")
-# x[2].plot(signal_avg[0:2000])
-# x[2].title.set_text("Moving Average")
-# x[3].plot(detected_signal[0:2000])
-# x[3].title.set_text("Detected")
-# plt.show()
-
-# fig, x = plt.subplots(4,1)
-# x[0].plot(original_signal)
+# x[0].plot(original_signal[start:end])
 # x[0].title.set_text("Original Signal")
 # x[1].plot(signal_rectify)
+# # x[1].plot(signal_rectify[start:end])
 # x[1].title.set_text("Rectify")
 # x[2].plot(signal_avg)
+# # x[2].plot(signal_avg[start:end])
 # x[2].title.set_text("Moving Average")
-# x[3].plot(detected_signal)
-# x[3].title.set_text("Detected")
+# x[3].plot(detection_signal)
+# # x[3].plot(detection_signal[start:end])
+# x[3].title.set_text("Det")
 # plt.show()
